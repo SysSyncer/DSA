5e8b0dca975eda6e71d88b923fcd6f9b915df872
SysSyncer <harikrishnan4665@gmail.com>
2025-11-23 00:13:28 +0530

    string copy(strcpy, strncpy)

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
   char source[] = "This is a string";
-  char destination[strlen(source)];
+  size_t length = strlen(source);
+
+  char destination[length];
+  char substrcpy[11];
+
   strcpy(destination, source);
+  strncpy(substrcpy, source, 10);
+
   printf("SOURCE: %s\nDESTINATION: %s\n", source, destination);
+  printf("\n%s\n", substrcpy);
+
   return EXIT_SUCCESS;
 }

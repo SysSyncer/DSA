4f3ba6b9b6f2af477985321ed4a49f12fc838f4b
SysSyncer <harikrishnan4665@gmail.com>
2025-11-23 00:54:46 +0530

    substring search by char: strchr(string, char), strrchr(string, char)

--- main.c
+++ main.c
@@ -4,9 +4,8 @@
 
 int main(int argc, char *argv[]) {
   char str[] = "Hello World | This is a Trap";
-  char *ptr = strstr(str, "Wor");
-  printf("%s\n", ptr);
+  printf("%s\n", strchr(str, 'a'));  // a Trap
+  printf("%s\n", strrchr(str, 'a')); // ap
 
-  // Output: World | This is a Trap
   return EXIT_SUCCESS;
 }

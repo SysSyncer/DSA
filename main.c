64165bcca2f809bb397d8de1496ace7d5c804e91
SysSyncer <harikrishnan4665@gmail.com>
2025-11-22 23:58:34 +0530

    cli arguments

--- main.c
+++ main.c
@@ -1,25 +1,10 @@
-#include <stddef.h>
 #include <stdio.h>
-#include <string.h>
-
-char *strrev(char *str) {
-  int first = 0;
-  size_t length = strlen(str);
-  int last = length - 1;
-  while (first < last) {
-    char temp = str[first];
-    str[first] = str[last];
-    str[last] = temp;
-    first++;
-    last--;
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+  int idx = 1;
+  while (idx < argc && argv[idx] != (void *)0) {
+    printf("%s\t", argv[idx++]);
   }
-  return str;
-}
-
-int main() {
-  char str[] = "Hello";
-  size_t length = sizeof(str) / sizeof(str[0]);
-  strrev(str);
-  printf("%s\n", strrev(str));
-  return 0;
+  printf("\n");
+  return EXIT_SUCCESS;
 }

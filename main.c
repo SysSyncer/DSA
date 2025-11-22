c50f1d74b9788f1e4d82bda2e5b96cf0b4bc40f8
SysSyncer <harikrishnan4665@gmail.com>
2025-11-23 00:47:40 +0530

    substring search: strstr(haystack | string, needle | substring)

--- main.c
+++ main.c
@@ -1,19 +1,12 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  char source[] = "This is source.";
-  char destination[50] = "This is destination.";
-  char substrcat[50] = "This is substr.";
+  char str[] = "Hello World | This is a Trap";
+  char *ptr = strstr(str, "Wor");
+  printf("%s\n", ptr);
 
-  strcat(destination, source);
-  strncat(substrcat, source, 4);
-
-  printf("%s\n", destination);
-  printf("%s\n", substrcat);
-  // Output: This is destination. This is source
-
+  // Output: World | This is a Trap
   return EXIT_SUCCESS;
 }
